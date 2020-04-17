aa01cb270353f2011b234dfa4e99e7a0634153ee
MrLinux <47211530+MrLinux@users.noreply.github.com>
2020-04-17 15:56:43 +0300

    lk

--- venv/Scripts/dmq.py
+++ venv/Scripts/dmq.py
@@ -2,10 +2,12 @@ import requests
 import time
 import sys
 
+
 def translate():
     url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?'
     key = 'trnsl.1.1.20200409T191416Z.e9897b968ea45216.fc831592f623388daee7313eed3091868362dacc'
-    r = requests.get("https://translate.yandex.net/api/v1.5/tr/getLangs", data={"key" : key})
+    r = requests.get("https://translate.yandex.net/api/v1.5/tr/getLangs", data={"key": key})
     print(r.text.replace("string", ""))
+
 
 translate()
